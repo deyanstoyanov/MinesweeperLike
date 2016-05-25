6b038151a0584f5a2590edb3d3100aed9c3937b4
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-05-25 15:21:31 +0300

    Added application icon and changed game form name

--- MinesweeperLike/MinesweeperLike.App/Forms/GameForm.Designer.cs
+++ MinesweeperLike/MinesweeperLike.App/Forms/GameForm.Designer.cs
@@ -28,13 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameForm));
+            this.SuspendLayout();
+            // 
+            // GameForm
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "GameForm";
-            this.Text = "GameForm";
+            this.Text = "MinesweeperLike";
             this.ResumeLayout(false);
+
         }
         #endregion
     }
