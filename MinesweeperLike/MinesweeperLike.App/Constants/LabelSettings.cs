10d956a16e08d6699e8db17468194237c102e6de
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-05-28 15:33:32 +0300

    Added default label settings: back color, ClickedOnMine color.

--- MinesweeperLike/MinesweeperLike.App/Constants/LabelSettings.cs
+++ MinesweeperLike/MinesweeperLike.App/Constants/LabelSettings.cs
@@ -1,11 +1,17 @@
 ﻿namespace MinesweeperLike.App.Constants
 {
-    public class LabelSettings
+    using System.Drawing;
+
+    public static class LabelSettings
     {
         public const int LabelFontSize = 12;
 
         public const int LabelSizeWidth = 21;
 
         public const int LabelSizeHeight = 21;
+
+        public static Color LabelBackColor = Color.FromArgb(228, 228, 228);
+
+        public static Color LabelClickedOnMineColor = Color.FromArgb(255, 0, 0);
     }
 }
