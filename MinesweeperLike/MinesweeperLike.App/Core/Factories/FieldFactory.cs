d92026cf9460653479c22e669133838b7e36e0ae
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-05-28 18:04:04 +0300

    Customize the Panel control. Handle Paint event and using ControlPaint.DrawBorder draw a thick 3D border.

--- MinesweeperLike/MinesweeperLike.App/Core/Factories/FieldFactory.cs
+++ MinesweeperLike/MinesweeperLike.App/Core/Factories/FieldFactory.cs
@@ -8,19 +8,32 @@
 
     public class FieldFactory : IFieldFactory
     {
+        private Panel panel;
+
         public Panel CreateGameField(int gameFieldWidth, int gameFieldHeight)
         {
-            Panel newPanel = new Panel
-                                 {
-                                     Location = new Point(
-                                         FieldSettings.GameFieldLocationWidth, 
-                                         FieldSettings.GameFieldLocationHeight), 
-                                     Height = (gameFieldWidth * ButtonSettings.ButtonSizeWidth) + 4, 
-                                     Width = (gameFieldHeight * ButtonSettings.ButtonSizeHeight) + 4, 
-                                     BorderStyle = BorderStyle.Fixed3D
-                                 };
+            this.panel = new Panel
+                                {
+                                    Location = new Point( 
+                                        FieldSettings.GameFieldLocationWidth, 
+                                        FieldSettings.GameFieldLocationHeight), 
+                                    Height = (gameFieldWidth * ButtonSettings.ButtonSizeWidth) + 8, 
+                                    Width = (gameFieldHeight * ButtonSettings.ButtonSizeHeight) + 8, 
+                                    BorderStyle = BorderStyle.None
+                                };
 
-            return newPanel;
+            this.panel.Paint += this.DrawThickBorder;
+
+            return this.panel;
+        }
+
+        private void DrawThickBorder(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, this.panel.ClientRectangle, 
+                FieldSettings.GameFieldBorderColor, FieldSettings.GameFieldBorderSize, ButtonBorderStyle.Solid, 
+                FieldSettings.GameFieldBorderColor, FieldSettings.GameFieldBorderSize, ButtonBorderStyle.Solid, 
+                SystemColors.ControlLightLight, FieldSettings.GameFieldBorderSize, ButtonBorderStyle.Solid, 
+                SystemColors.ControlLightLight, FieldSettings.GameFieldBorderSize, ButtonBorderStyle.Solid);
         }
     }
 }
