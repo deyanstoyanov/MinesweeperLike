99c47b50d248890b3f2ff623fce536bf242cfe76
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-05-28 14:56:54 +0300

    Added default button settings: back color, border color and border size.

--- MinesweeperLike/MinesweeperLike.App/Constants/ButtonSettings.cs
+++ MinesweeperLike/MinesweeperLike.App/Constants/ButtonSettings.cs
@@ -1,6 +1,8 @@
 ﻿namespace MinesweeperLike.App.Constants
 {
-    public class ButtonSettings
+    using System.Drawing;
+
+    public static class ButtonSettings
     {
         public const int ButtonSizeWidth = 20;
 
@@ -9,5 +11,11 @@
         public const int WindowLocationWidth = 3;
 
         public const int WindowLocationHeight = 3;
+
+        public const int ButtonBorderSize = 2;
+
+        public static Color ButtonBorderColor = Color.FromArgb(117, 117, 117);
+
+        public static Color ButtonBackColor = Color.FromArgb(240, 240, 240);
     }
 }
