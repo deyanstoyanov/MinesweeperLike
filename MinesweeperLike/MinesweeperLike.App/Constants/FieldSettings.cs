ff211e39ad143fe8c96de02742234ffd6f8ce0a4
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-05-28 15:14:41 +0300

    Added default game field settings: border color and border size.

--- MinesweeperLike/MinesweeperLike.App/Constants/FieldSettings.cs
+++ MinesweeperLike/MinesweeperLike.App/Constants/FieldSettings.cs
@@ -2,16 +2,20 @@
 {
     using System.Drawing;
 
-    public class FieldSettings
+    public static class FieldSettings
     {
         public const int GameFieldLocationWidth = 29;
 
         public const int GameFieldLocationHeight = 52;
+
+        public const int GameFieldBorderSize = 3;
 
         public const string FlagImagePath = @"../../Resources/flag.png";
 
         public const string NotAMinePath = @"../../Resources/not-a-mine.png";
 
         public const string Font = "Arial";
+
+        public static readonly Color GameFieldBorderColor = Color.FromArgb(117, 117, 117);
     }
 }
